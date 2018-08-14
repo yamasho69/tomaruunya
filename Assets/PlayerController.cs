cc1e60940d75bea5b393f35910071e9e1c58dbdc
shoheiyamamoto <nitiham55@yahoo.co.jp>
2018-08-15 00:15:59 +0900

    4回目
    プレイヤーコントローラーを修正、キャラクターコントローラー等を削除

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,17 +2,16 @@
 using UnityStandardAssets.CrossPlatformInput;
 
 
-public class PlayerController: MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     // このスクリプトで使う変数一覧
     private CharacterController charaCon;       // キャラクターコンポーネント用の変数
     private Animator animCon;  //  アニメーションするための変数
     //プレイヤーを移動させるコンポーネントを入れる
-    private Rigidbody RigiCon;
-    public float idoSpeed = 5.0f;         // 移動速度（Public＝インスペクタで調整可能）
-    public float kaitenSpeed = 1200.0f;   // プレイヤーの回転速度（Public＝インスペクタで調整可能）
+    private Rigidbody rigicon;
     //前進するための力
-    private float forwardForce = 800.0f;
+    public float forwardForce = 3.0f;
+    public float rotateSpeed = 20.0f;
     //ジャンプするための力
     private float upForce = 500.0f;
     //動きを減速させる係数
@@ -28,29 +27,43 @@ public class PlayerController: MonoBehaviour
         charaCon = GetComponent<CharacterController>(); // キャラクターコントローラーのコンポーネントを参照する
         animCon = GetComponent<Animator>(); // アニメーターのコンポーネントを参照する
         //走るアニメーションを開始
-        this.animCon.SetFloat("Speed", 1);
+        //this.animCon.SetFloat("Speed", 1);
         //Rigidbodyコンポーネントを取得
-        this.RigiCon = GetComponent<Rigidbody>();
+        this.rigicon = GetComponent<Rigidbody>();
     }
     // ■毎フレーム常に実行する処理
     void Update()
     {
+        float v = CrossPlatformInputManager.GetAxisRaw("Vertical");
+        float h = CrossPlatformInputManager.GetAxisRaw("Horizontal");
+        Rigidbody rb = GetComponent<Rigidbody>();
+        //rb.AddForce(v * forwardForce * transform.forward, ForceMode.Force);   // 上下で前進・後退
+        if (CrossPlatformInputManager.GetButton("Fire2"))   // Fire2 = 右クリック、左 Alt
+        {
+            rb.velocity = rb.velocity * coefficient;
+        }
+        else
+        {
+            rb.AddForce(forwardForce * transform.forward, ForceMode.Force);
+        }
+        transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed * h);
+
+        //前に行く力を加える
+        this.rigicon.AddForce(this.transform.forward * this.forwardForce);
+
         // ▼▼▼移動処理▼▼▼
         if (CrossPlatformInputManager.GetAxisRaw("Vertical") == 0 && CrossPlatformInputManager.GetAxisRaw("Horizontal") == 0) //  テンキーや3Dスティックの入力（GetAxis）がゼロの時の動作
         {
-            animCon.SetBool("A_dash", false);  //  Runモーションしない
+            animCon.SetBool("is_dash", false);  //  Runモーションしない
         }
         else //  テンキーや3Dスティックの入力（GetAxis）がゼロではない時の動作
         {
             var cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;  //  カメラが追従するための動作
             Vector3 direction = Camera.main.transform.right * CrossPlatformInputManager.GetAxisRaw("Horizontal");  //  テンキーや3Dスティックの入力（GetAxis）があるとdirectionに値を返す
-            animCon.SetBool("A_dash", true);  //  Runモーションする
-
-            MukiWoKaeru(direction);  //  向きを変える動作の処理を実行する（後述）
-            IdoSuru(direction);  //  移動する動作の処理を実行する（後述）
+            animCon.SetBool("is_dash", true);  //  Runモーションする
         }
     }
-    void OnTriggerEnter(Collider other)
+    void OnCollisionStay(Collider other)
     {
         //ジャンプパネル接触時にアクションボタンを押下したとき
         if (other.gameObject.tag == "Jump" && Input.GetButtonDown("Action"))
@@ -58,12 +71,12 @@ public class PlayerController: MonoBehaviour
             //ジャンプアニメを再生
             animCon.SetBool("Jump", true);
             //プレイヤーに上方向の力を加える
-            RigiCon.AddForce(this.transform.up * this.upForce);
+            rigicon.AddForce(this.transform.up * this.upForce);
         }
         //ダッシュパネル接触時にアクションボタンを押下したとき
         if (other.gameObject.tag == "Dash" && Input.GetButtonDown("Action"))
-        {　
-        　　//加速する
+        {
+            //加速する
 
         }
 
@@ -71,7 +84,7 @@ public class PlayerController: MonoBehaviour
     //ブレーキボタンを押下したとき
     public void GetMyBrakeButtonDown()
     {
-　　　　　　//地面に設置していれば
+        //地面に設置していれば
         if (this.transform.position.y < 0.5f)
         {
             //減速する
@@ -79,18 +92,5 @@ public class PlayerController: MonoBehaviour
             this.upForce *= this.coefficient;
             this.animCon.speed *= this.coefficient;
         }
-    }
-
-
-    // ■向きを変える動作の処理
-    void MukiWoKaeru(Vector3 mukitaiHoukou)
-    {
-        Quaternion q = Quaternion.LookRotation(mukitaiHoukou);          // 向きたい方角をQuaternion型に直す
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, q,  kaitenSpeed * 1/850);   // 向きを q に向けてじわ～っと変化させる.
-    }
-    // ■移動する動作の処理
-    void IdoSuru(Vector3 idosuruKyori)
-    {
-        charaCon.Move(idosuruKyori * Time.deltaTime * idoSpeed);   // プレイヤーの移動距離は時間×移動スピードの値
     }
 }
