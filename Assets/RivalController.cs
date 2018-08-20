8914777f77831abd65b2190504b4e10046c944ee
shoheiyamamoto <nitiham55@yahoo.co.jp>
2018-08-21 01:52:56 +0900

    11回目
    ライバルの動き実装、最後の柵附近の動きを見直す必要あり

---
+++ Assets/RivalController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class RivalController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] m_targets = null;
+    [SerializeField]
+    private float m_destinationThreshold = 0.0f;
+
+    private NavMeshAgent m_navAgent = null;
+
+    private int m_targetIndex = 0;
+
+    private Vector3 CurretTargetPosition
+    {
+        get
+        {
+            if (m_targets == null || m_targets.Length <= m_targetIndex)
+            {
+                return Vector3.zero;
+            }
+
+            return m_targets[m_targetIndex].position;
+        }
+    }
+
+    private void Start()
+    {
+        m_navAgent = GetComponent<NavMeshAgent>();
+        m_navAgent.destination = CurretTargetPosition;
+    }
+
+    private void Update()
+    {
+        if (m_navAgent.remainingDistance <= m_destinationThreshold)
+        {
+            m_targetIndex = (m_targetIndex + 1) % m_targets.Length;
+
+            m_navAgent.destination = CurretTargetPosition;
+        }
+    }
+
+} // class ObjectController
+
+
+//{public Transform target;
+//NavMeshAgent agent;
+
+//void Start()//{
+//agent = GetComponent<NavMeshAgent>();}
+//void Update(){agent.SetDestination(target.position);}}
