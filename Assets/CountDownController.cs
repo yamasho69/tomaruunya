251171e6f2b1d8bb63289c926c632c7a0a4c46d1
shoheiyamamoto <nitiham55@yahoo.co.jp>
2018-08-29 21:47:02 +0900

    19.4回目
    カウントダウン実装。カウントダウン後に走り出す処理を残すのみ。

---
+++ Assets/CountDownController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CountDownController : MonoBehaviour
+{
+    public GameObject _textCountdown;
+    AudioSource audioSource;//オーディオコンポーネント
+    public AudioClip Count;
+    public AudioClip Go;
+
+    IEnumerator Start()
+    {
+        //_textCountdown.text = "";
+        //_textCountdown.gameObject.SetActive(true);
+        audioSource = gameObject.GetComponent<AudioSource>();
+
+        this._textCountdown.GetComponent<Text>().text = "３";
+        audioSource.PlayOneShot(Count, 1.0f);
+        yield return new WaitForSeconds(1.0f);
+
+        this._textCountdown.GetComponent<Text>().text = "２";
+        audioSource.PlayOneShot(Count, 1.0f);
+        yield return new WaitForSeconds(1.0f);
+
+        this._textCountdown.GetComponent<Text>().text = "１";
+        audioSource.PlayOneShot(Count, 1.0f);
+        yield return new WaitForSeconds(1.0f);
+
+        this._textCountdown.GetComponent<Text>().text = "すたーと㍉";
+        audioSource.PlayOneShot(Go, 1.0f);
+        yield return new WaitForSeconds(1.0f);
+
+        this._textCountdown.GetComponent<Text>().text = "";
+    }
+    public void Update()
+    {
+        
+    }
+}
